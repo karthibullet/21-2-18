e5c9d26e31470108af7d99b0d2506cac2fae579d
karthibullet <35765123+karthibullet@users.noreply.github.com>
2018-02-21 16:05:49 +0530

    44.c

--- 44.c
+++ 44.c
@@ -1,15 +1,15 @@
+
 #include <stdio.h>
-int main(void) 
+ int main()
 {
-int n;
-scanf("%d",&n);
-if(n>=1 && n<=10)
-{
-printf("YES");
-}
-else
-{
-printf("NO");
-}
-return 0;
-}
+  int Number, Reminder, Count=0;
+  printf("\n Please Enter any number\n");
+  scanf("%d", &Number);
+  while(Number > 0)
+  {
+  Number = Number / 10;
+  Count = Count + 1;  
+  }
+  printf("\n Number of Digits in a Given Number = %d", Count);
+  return 0;
+  }
