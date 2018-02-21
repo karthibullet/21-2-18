1c8419178d5fb3aae8c9257377facfb9532530f0
karthibullet <35765123+karthibullet@users.noreply.github.com>
2018-02-21 15:53:43 +0530

    43.c

---
+++ 43.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+void main()
+{
+char s1[30],s2[30],i,count=0,j=0;
+printf("enter the strings");
+scanf("%s%s",&s1,&s2);
+for(i=0;s1[i]!='\0';i++)
+{
+count++;
+}
+for(i=--count;s2[i]!='\0';i++)
+{
+s1[i]=s2[j];
+}
+printf("string is %s",s1);
+}
